41d3cdd0de37539b8c2dbabc48e9ab93f0a1461b
jw426 <jiwonbae010426@gmail.com>
2024-08-08 14:15:11 +1000

    skeleton for wk3

---
+++ wk3/ex4.06.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    
+    // Take in input / Read in characters (getchar())
+    
+    // print out results (printf)
+    
+    return 0;   
+}
