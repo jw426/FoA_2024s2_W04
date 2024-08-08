2b3dd5be7b59d184fe9f3eb7fe83c84644b5ef37
jw426 <jiwonbae010426@gmail.com>
2024-08-08 15:31:40 +1000

    skeleton

--- wk3/ex4.06.c
+++ wk3/ex4.06.c
@@ -3,7 +3,13 @@
 int main(int argc, char** argv) {
     
     // Take in input / Read in characters (getchar())
-    
+    char ch; 
+    int num_lines = 0, num_chars = 0;
+    while ((ch = getchar()) != EOF) {
+
+    }
+
+
     // print out results (printf)
     
     return 0;   
