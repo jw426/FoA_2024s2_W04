652bd048a4df97237fb048f252718df0eedb2934
jw426 <jiwonbae010426@gmail.com>
2024-08-15 13:57:08 +1000

    wk 4 skeleton

---
+++ wk4/ex6.09.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COIN1     200
+#define COIN2     100
+#define COIN3     50
+#define COIN4     20
+#define COIN5     10
+#define COIN6     5
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
+/**
+ * function that converts cents to required changes.
+ * tests each different coin denomination in the correct ordering, 
+ * and uses 'try_one_coin' to tell it how to generate the required change.
+ */
+void print_change(int cents) {
+
+
+ 
+}
+
+/**
+ * reduces the current cents amount 
+ * by the value of the current coin as many times as is possible, 
+ * altering the value of cents appropriately 
+ * and returning the number of coins of that denomination that should be issued
+ */
+int try_one_coin(int *cents, int coin) {
+    
+    
+}
+
+/**
+ * function that prints the result
+ * calculated by 'try_one_coin' to the terminal
+ */
+void print_result(int num, int coin) {
+    printf("give %d %3d-cent coins\n", num, coin);
+}
+
+
+int round_to_5(int cents) {
+
+    
+}
+    
