bb21f30283f036b56067faf50400f6ba14fb098a
jw426 <jiwonbae010426@gmail.com>
2024-08-20 15:59:26 +1000

    scaffold for wk5

---
+++ wk5/ex7.06.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "arrayops.h"
+
+// Maximum size of numbers array
+// #define
+
+/* function prototypes */
+
+
+int
+main(int argc, char *argv[]) {
+    
+    return 0;
+}
+
+/* returns index of smallest value in the supplied array
+ * another name for this function is argmax 
+ * iterative version */
+int
+index_of_smallest(int A[], int n) {
+
+}
+
+/* implements selection sort by repeatedly swapping
+   the next smallest item in the array into the first position of a
+   decreasing array segment
+   iterative version
+*/
+void
+selection_sort(int A[], int n) {
+
+}
